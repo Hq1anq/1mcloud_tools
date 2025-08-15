145d0ce6fb1974e10c412c4cd980641b678cef75
Giang <trinhhoanggiang05@gmail.com>
2025-08-15 23:28:24 +0700

    adjust home route

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,22 +1,15 @@
 import express from 'express';
 import { startProxyCheckStream, receiveProxies } from '../controllers/proxyChecker.js';
 import { getData, changeIp, reinstall, changeNote, pause } from '../controllers/proxyManager.js';
-import { getHomePage, postCreateUser, getCreatePage, getUpdatePage } from '../controllers/home.js';
 
 const router = express.Router();
 
-router.get('/', getHomePage);
-router.get('/create', getCreatePage);
-router.get('/update', getUpdatePage);
 router.get('/proxy/check-stream', startProxyCheckStream);
-
-router.post('/create-user', postCreateUser);
-router.post('/getData', getData);
 router.post('/proxy/send-proxies', receiveProxies);
 
+router.post('/getData', getData);
 router.post('/proxy/change-note', changeNote);
 router.post('/proxy/pause', pause);
-
 router.post('/proxy/change-ip', changeIp);
 router.post('/proxy/reinstall', reinstall);
 

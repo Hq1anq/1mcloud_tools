7aea3a505953f71e02d881903c5628e0a45f9e43
Giang <trinhhoanggiang05@gmail.com>
2025-10-03 13:56:43 +0700

    enhance toast message, update status cell after renew

--- src/public/javascript/proxyManager.js
+++ src/public/javascript/proxyManager.js
@@ -783,19 +783,22 @@ async function pause() {
 			// Show appropriate toast message
 			if (errorIds.length === 0)
 				changeToToast(
-					`PAUSE completed ${successIds.length} success`,
+					`PAUSE completed <br> 
+					<span class="text-text-toast-success">${successIds.length} success</span>`,
                     "Pausing",
 					"success",
 				);
 			else if (successIds.length === 0)
 				changeToToast(
-					`PAUSE failed for <span class="text-text-toast-error">${errorIds.length}</span> servers`,
+					`PAUSE completed <br>
+					<span class="text-text-toast-error">${errorIds.length} failed</span>`,
                     "Pausing",
 					"error",
 				);
 			else
 				changeToToast(
-					`PAUSE completed: ${successIds.length} success, ${errorIds.length} failed`,
+					`PAUSE completed <br>
+					<span class="text-text-toast-success">${successIds.length} success</span>, <span class="text-text-toast-error">${errorIds.length} failed</span>`,
                     "Pausing",
 					"warning",
 				);
@@ -868,7 +871,8 @@ async function reboot() {
 				);
 			else if (successIds.length === 0)
 				changeToToast(
-					`Reboot failed for <span class="text-text-toast-error">${errorIds.length}</span> servers`,
+					`Reboot completed <br>
+					<span class="text-text-toast-error">${errorIds.length} failed</span>`,
 					"REBOOT...",
                     "error",
 				);
@@ -940,19 +944,22 @@ async function refund() {
 			// Show appropriate toast message
 			if (errorIPs.length === 0)
 				changeToToast(
-					`REFUND completed ${successIPs.length} success`,
+					`REFUND completed <br>
+					<span class="text-text-toast-success">${successIPs.length} success</span>`,
                     "REFUND...",
 					"success",
 				);
 			else if (successIPs.length === 0)
 				changeToToast(
-					`REFUND failed for <span class="text-text-toast-error">${errorIPs.length}</span> servers`,
+					`REFUND completed <br>
+					<span class="text-text-toast-error">${errorIPs.length} failed</span>`,
                     "REFUND...",
 					"error",
 				);
 			else
 				changeToToast(
-					`REFUND completed: ${successIPs.length} success, ${errorIPs.length} failed`,
+					`REFUND completed <br>
+					<span class="text-text-toast-success">${successIPs.length} success</span>, <span class="text-text-toast-error">${errorIPs.length} failed</span>`,
 					"REFUND...",
                     "warning",
 				);
@@ -1017,19 +1024,22 @@ async function renew() {
 			// Show appropriate toast message
 			if (errorIPs.length === 0)
 				changeToToast(
-					`RENEW completed ${successIPs.length} success`,
+					`RENEW completed <br>
+					<span class="text-text-toast-success">${successIPs.length} success</span>`,
                     "RENEW...",
 					"success",
 				);
 			else if (successIPs.length === 0)
 				changeToToast(
-					`RENEW failed for <span class="text-text-toast-error">${errorIPs.length}</span> servers`,
+					`RENEW completed <br>
+					<span class="text-text-toast-error">${errorIPs.length} failed</span>`,
                     "RENEW...",
 					"error",
 				);
 			else
 				changeToToast(
-					`RENEW completed: ${successIPs.length} success, ${errorIPs.length} failed`,
+					`RENEW completed <br>
+					<span class="text-text-toast-success">${successIPs.length} success</span>, <span class="text-text-toast-error">${errorIPs.length} failed</span>`,
 					"RENEW...",
                     "warning",
 				);
@@ -1152,7 +1162,8 @@ async function changeNote() {
 		);
 	else if (successCount === 0)
 		changeToToast(
-			`CHANGE NOTE failed for <span class="text-text-toast-error">${failCount}</span> servers`,
+			`CHANGE NOTE completed <br>
+			<span class="text-text-toast-error">${failCount} failed</span>`,
 			"Changing Note",
             "error",
 		);
@@ -1191,6 +1202,7 @@ function updateRowContent(row, text, action) {
 		date.setDate(date.getDate() + 30);
 		const newExpired = date2str(date);
 		cells[columnMap.expired].innerHTML = newExpired;
+		cells[columnMap.status].innerHTML = getStatusChip("Running");
 		updateRowData(id, {
 			expired: newExpired,
 			status: "Running"
